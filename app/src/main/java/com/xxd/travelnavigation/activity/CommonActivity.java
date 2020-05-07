13b83183e070a13d41268d01e9c777469e6e9d93
xxd <txgsqy@163.com>
2020-05-08 03:51:50 +0800

    测试2

--- app/src/main/java/com/xxd/travelnavigation/activity/CommonActivity.java
+++ app/src/main/java/com/xxd/travelnavigation/activity/CommonActivity.java
@@ -24,7 +24,7 @@ public class CommonActivity extends AppCompatActivity {
 //            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 //            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 //            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);、、
+//            window.setStatusBarColor(Color.TRANSPARENT);、、··
 
 //        }
     }
