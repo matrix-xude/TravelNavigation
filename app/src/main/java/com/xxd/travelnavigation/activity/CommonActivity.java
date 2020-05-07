72876d1b6300802c450c96bbc284595cc46bf74a
xxd <txgsqy@163.com>
2020-05-08 04:00:56 +0800

    测试3

--- app/src/main/java/com/xxd/travelnavigation/activity/CommonActivity.java
+++ app/src/main/java/com/xxd/travelnavigation/activity/CommonActivity.java
@@ -25,6 +25,7 @@ public class CommonActivity extends AppCompatActivity {
 //            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 //            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 //            window.setStatusBarColor(Color.TRANSPARENT);、、··
+///  Laiyihangxinde 
 
 //        }
     }
